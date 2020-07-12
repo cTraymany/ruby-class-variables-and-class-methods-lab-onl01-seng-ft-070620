9713ec783637eb7a6c18c2fc6bfb09f6d33890c3
Chindalath Traymany <chindalatht@gmail.com>
2020-07-12 08:46:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+    
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    genre_array = []
+    @@genres.each do |genre|  
+      unless genre_array.include?(genre)
+        genre_array << genre
+      end
+    end
+    genre_array
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      unless genre_hash[genre]
+        
+      end
+    end    
+  end
+  
+  def artist_count
+    @@artists
+  end
+  
+
+end
