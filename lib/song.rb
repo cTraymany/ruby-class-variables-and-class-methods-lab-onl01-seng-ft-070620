d204d14b2369cb871eb4b222b0d2b632ca57d75c
Chindalath Traymany <chindalatht@gmail.com>
2020-07-12 09:08:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
     genre_hash = {}
     
     @@genres.each_with_index do |genre, count|
-      count = 0
       if !genre_hash[genre]
+        count = 0
         genre_hash[genre] = count += 1
       elsif genre_hash.include?(genre)
          count += 1
